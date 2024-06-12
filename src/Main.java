b2a115f09682452c7bddeada918e64d06643c813
Sergey Shkaev <shkaev.ss@mail.ru>
2024-06-12 13:24:23 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("комит 1");
+        System.out.println("комит 2");
     }
 }
