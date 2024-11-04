e42a47259bd1c16a27c46885aff0bf32ac0d894a
serg <shkaev.ss@mail.ru>
2024-11-04 21:25:30 -0200

    проверил. Всё супер.

--- src/Cat.java
+++ src/Cat.java
@@ -4,4 +4,12 @@ public class Cat {
   public Cat(String name) {
     this.name = name;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-   Cat cat = new Cat("Tom");
+    Cat cat = new Cat("Tom");
+    System.out.println(cat.getName());
   }
 }
